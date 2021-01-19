723980aeb3f287845f1e3f54560247ea01db432d
Ilson <ilson@MacBook-Air-de-MaurilioJr.local>
2021-01-19 17:40:19 -0300

    add service-worker

--- src/index.js
+++ src/index.js
@@ -8,12 +8,17 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { PersistGate } from 'redux-persist/integration/react';
 import WebfontLoader from '@dr-kobros/react-webfont-loader';
+import { ToastContainer, toast } from "react-toastify";
 
 // Internal
 import Root from './routes/Root';
 import { store, persistor } from './state/configureStore';
 import history from './routes/history';
+import * as serviceWorker from './service-worker';
 import { ConnectedRouter } from 'connected-react-router';
+
+// style
+import 'react-toastify/dist/ReactToastify.css';
 
 const config = {
   google: {
@@ -33,20 +38,24 @@ const muiTheme = getMuiTheme({
 const render = () => {
   ReactDOM.render((
     <AppContainer>
-      <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <WebfontLoader config={config}>
-          <MuiThemeProvider muiTheme={muiTheme}>
-          <ConnectedRouter history={history}>
-              <Root />
-              </ConnectedRouter>
-          </MuiThemeProvider>
-        </WebfontLoader>
-      </PersistGate>
-      </Provider>
+      <>
+        <ToastContainer style={{zIndex: 9999}}/>
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <WebfontLoader config={config}>
+              <MuiThemeProvider muiTheme={muiTheme}>
+              <ConnectedRouter history={history}>
+                  <Root />
+                  </ConnectedRouter>
+              </MuiThemeProvider>
+            </WebfontLoader>
+          </PersistGate>
+        </Provider>
+      </>
     </AppContainer>
   ), document.getElementById('root'));
 };
 
 // initial render
 render();
+serviceWorker.register({ toast });
